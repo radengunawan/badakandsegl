13e9074d8e73bfaa09ec9affe161711dae8826f3
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-21 09:19:33 +0800

    Add project files.

---
+++ HippoAndWolf/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace HippoAndWolf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*if using array*/
+            /*
+                Animal[] animals =
+                {
+            new Wolf(false),
+            new Hippo(),
+            new Wolf(true),
+            new Wolf(false),
+            new Hippo()
+            };
+            */
+
+            //using list
+            List<Animal> animals = new List<Animal>()
+        {
+                new Wolf(false),
+            new Hippo(),
+            new Wolf(true),
+            new Wolf(false),
+            new Hippo()
+        };
+
+            foreach (Animal animal in animals)
+            {
+                animal.MakeNoise();
+
+                if (animal is ISwimmer swimmer)
+                {
+                    swimmer.Swim();
+                }
+
+                if (animal is IPackHunter hunter)
+                {
+                    hunter.HuntInPack();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
